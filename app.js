7c7148545b436da20ba5dd3181a03adbba26272a
Italos Estilon <italosestilon@gmail.com>
2016-09-28 22:30:07 -0300

    Improve code quality.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 
 var eventApi= require("./routes/event-api");
 
@@ -17,7 +17,6 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", eventApi);
 
